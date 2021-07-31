1af1337cceddec6aea0b8853460d253ea91289b0
summerbythewater <87761963+summerbythewater@users.noreply.github.com>
2021-07-31 17:49:33 +0200

    Added date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`;
+
+}
+
 function displayTemp(response) {
     console.log(response);
     let tempElement = document.querySelector("#big-temp");
@@ -12,6 +28,8 @@ function displayTemp(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "fdeebaf7e5260bc95803113646d25a38";
